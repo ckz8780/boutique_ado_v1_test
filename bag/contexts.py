49fdd4220c1c36ccb9cc137799f3f1dd7995e774
Chris Z <ckz8780@gmail.com>
2020-02-17 00:35:07 +0000

    added checkout app, models and admit

--- bag/contexts.py
+++ bag/contexts.py
@@ -8,7 +8,7 @@ def bag_contents(request):
     """
     Ensures that the bag contents are available when rendering every page
     """
-    
+
     bag_items = []
     total = 0
     product_count = 0
@@ -44,7 +44,7 @@ def bag_contents(request):
         free_delivery_delta = 0
 
     grand_total = delivery + total
-    
+
     context = {
         'bag_items': bag_items,
         'total': total,
@@ -54,5 +54,5 @@ def bag_contents(request):
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
-    
+
     return context
