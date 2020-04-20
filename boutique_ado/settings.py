e7cb51a62ee447e277be8e045afaf4eefab0db0b
Chris Z <ckz8780@gmail.com>
2020-04-20 02:04:18 +0000

    Explicitly defined URLS

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -196,10 +196,14 @@ if 'USE_AWS' in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
-    STATICFILES_LOCATION = 'static'
-    MEDIAFILES_LOCATION = 'media'
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Explicitly define urls here to force HTTPS
+    STATIC_URL = AWS_S3_CUSTOM_DOMAIN + STATIC_URL
+    MEDIA_URL = AWS_S3_CUSTOM_DOMAIN + MEDIA_URL
 
 
 # Payments
