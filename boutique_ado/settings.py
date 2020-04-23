5b159e99198adc1b42c76824d4e0b5d584978dc7
Chris Z <ckz8780@gmail.com>
2020-04-23 19:27:24 +0000

    fixed TLS port

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -68,7 +68,7 @@ AUTHENTICATION_BACKENDS = (
 # Log emails to console in dev
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_SSL = True
+EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAL_HOST_USER')
