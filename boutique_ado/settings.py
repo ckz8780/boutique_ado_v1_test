49075ff6c4f5754111cbe341b42630ffdbe2dceb
Chris Z <ckz8780@gmail.com>
2020-04-23 21:01:45 +0000

    MAYBE IT WOULD HELP IF I COULD SPELL THE WORD E M A I L

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -68,12 +68,12 @@ AUTHENTICATION_BACKENDS = (
 # Log emails to console in dev
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_SSL = True
-EMAIL_PORT = 465
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get('EMAL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAL_HOST_PASS')
-DEFAULT_FROM_EMAIL = os.environ.get('EMAL_HOST_USER')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
