7a1b2ed8c9e77518040c7b936e515014c5e0feb9
Chris Z <ckz8780@gmail.com>
2020-04-23 20:33:12 +0000

    trying port 465

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -68,8 +68,8 @@ AUTHENTICATION_BACKENDS = (
 # Log emails to console in dev
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_USE_SSL = True
+EMAIL_PORT = 465
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAL_HOST_PASS')
